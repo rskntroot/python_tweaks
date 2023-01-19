e373fc74d54222fc627e4230a18bb2396f5cd965
rskntroot <56370891+rskntroot@users.noreply.github.com>
2023-01-18 18:00:35 -0700

    added support for if and for cases

--- revj2.sh
+++ revj2.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # not exactly python, but why do something complicated just to find why your j2 aint working
 
+echo "== base =="
 cat $1 | grep -oP "{{[^}]+}}" | sort | uniq | awk '{print $2}'
-echo "======"
-cat $1 | grep -oP "{%[^%]+%}" | sort | uniq | awk '{print $2}'
+echo "== if =="
+cat $1 | grep -oP "{%[^%]+%}" | sort | uniq | grep -oP "(?<=if\s)[^%\s]+"
+echo "== for =="
+cat $1 | grep -oP "{%[^%]+%}" | sort | uniq | grep -oP "(?<=for\s)[^%]+"
 
-#'lost'ju23% 
+# meh, i ended up over doing it anyway
+
+#'lost'ju23%
